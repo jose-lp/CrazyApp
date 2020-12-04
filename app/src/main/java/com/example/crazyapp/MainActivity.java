ffdae4b3976ddae9573b1b6b1518cce8aebdb256
Mariela Hdez <mariela.hdez.ch@gmail.com>
2020-12-04 17:35:18 -0600

    Test

--- app/src/main/java/com/example/crazyapp/MainActivity.java
+++ app/src/main/java/com/example/crazyapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.crazyapp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button blue_btn = (Button)findViewById(R.id.blue_btn);
+    Button selColor_btn = (Button)findViewById(R.id.selColor_btn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+
+    }
